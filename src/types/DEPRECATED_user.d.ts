8bc1e983e2130086fa9981d7fad7cac67afd267a
Enes Uğur <71911663+enesuur@users.noreply.github.com>
2025-05-16 01:28:36 +0300

    refactor:DEPRECATED types removed.

--- src/types/DEPRECATED_user.d.ts
+++ src/types/DEPRECATED_user.d.ts
@@ -1,11 +1,11 @@
 /* CAUTION: DO NOT USE THAT TYPE. THIS FILE IS DEPRECATED. */
-export interface IFavorite_Anime {
-  _id: string;
-  english_name: string | null;
-  japanese_name: string | null;
-  release_date: Date | null;
-  airing_date: Date | null;
-  ending_date: Date | null;
-  mal_url: string | null;
-  cover_img_url: string | null;
-},
+// export interface IFavorite_Anime {
+//   _id: string;
+//   english_name: string | null;
+//   japanese_name: string | null;
+//   release_date: Date | null;
+//   airing_date: Date | null;
+//   ending_date: Date | null;
+//   mal_url: string | null;
+//   cover_img_url: string | null;
+// },
